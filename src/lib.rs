f677051b3b7e893f1101bd0a11e943c029fac032
Alexander Sukharev <asuk1979@gmail.com>
2015-12-09 01:17:44 +0300

    Decode

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 const CHARMAP: [u8; 64] = [
     0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48,
     0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50,
@@ -9,52 +11,58 @@ const CHARMAP: [u8; 64] = [
     0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x2B, 0x2F
 ];
 
+fn encode(bytes: &[u8]) -> String {
+    let mut encoded_data: Vec<u8> = Vec::new();
+    for chunk in bytes.chunks(3) {
+        let word: u32 =
+            ((chunk[0] as u32) << 16) |
+            ((chunk[1] as u32) << 8) |
+             (chunk[2] as u32);
+        let mut out_chunk: [u8; 4] = [0; 4];
+        out_chunk[0] = (word >> 18) as u8;
+        out_chunk[1] = (word << 14 >> 26) as u8;
+        out_chunk[2] = (word << 20 >> 26) as u8;
+        out_chunk[3] = (word << 26 >> 26) as u8;
+        encoded_data.push(out_chunk[0]);
+        encoded_data.push(out_chunk[1]);
+        encoded_data.push(out_chunk[2]);
+        encoded_data.push(out_chunk[3]);
+    }
+    encoded_data.iter().map(|c| CHARMAP[*c as usize] as char).collect()
+}
+
+fn decode(string: &String) -> Vec<u8> {
+    let mut decoded_data: Vec<u8> = Vec::new();
+    let bytes = string.as_bytes();
+    for chunk in bytes.chunks(4) {
+        let mut restored_chunk: [u8; 4] = [0; 4];
+        restored_chunk[0] = CHARMAP.iter().position(|x| *x == chunk[0]).unwrap() as u8;
+        restored_chunk[1] = CHARMAP.iter().position(|x| *x == chunk[1]).unwrap() as u8;
+        restored_chunk[2] = CHARMAP.iter().position(|x| *x == chunk[2]).unwrap() as u8;
+        restored_chunk[3] = CHARMAP.iter().position(|x| *x == chunk[3]).unwrap() as u8;
+        let word: u32 =
+            ((restored_chunk[0] as u32) << 18) |
+            ((restored_chunk[1] as u32) << 12) |
+            ((restored_chunk[2] as u32) << 6) |
+             (restored_chunk[3] as u32);
+        let mut out_chunk: [u8; 3] = [0; 3];
+        out_chunk[0] = (word <<  8 >> 24) as u8;
+        out_chunk[1] = (word << 16 >> 24) as u8;
+        out_chunk[2] = (word << 24 >> 24) as u8;
+        decoded_data.push(out_chunk[0]);
+        decoded_data.push(out_chunk[1]);
+        decoded_data.push(out_chunk[2]);
+    }
+    decoded_data
+}
+
 #[test]
 fn it_works() {
-    print!("\nEncode");
-
     let string = "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure. ";
-    let bytes = string.as_bytes();
-    let mut edata: Vec<u8> = Vec::new();
-    for b in bytes.chunks(3) {
-        let word: u32 =
-            ((b[0] as u32) << 16) |
-            ((b[1] as u32) << 8) |
-            (b[2] as u32);
-        let mut bc: [u8; 4] = [0; 4];
-        bc[0] = (word >> 18) as u8;
-        bc[1] = (word << 14 >> 26) as u8;
-        bc[2] = (word << 20 >> 26) as u8;
-        bc[3] = (word << 26 >> 26) as u8;
-        edata.push(bc[0]);
-        edata.push(bc[1]);
-        edata.push(bc[2]);
-        edata.push(bc[3]);
-    }
-    print!("\n");
-    println!("{:?}", edata);
-    let s: String = edata.iter().map(|c| CHARMAP[*c as usize] as char).collect();
-    println!("{}", s);
+    let encoded_string = encode(&string.as_bytes());
 
-    println!("\nDecode");
-    let mut ddata: Vec<u8> = Vec::new();
-    for b in edata.chunks(4) {
-        let word: u32 =
-            ((b[0] as u32) << 18) |
-            ((b[1] as u32) << 12) |
-            ((b[2] as u32) << 6) |
-            (b[3] as u32);
-        let mut bc: [u8; 3] = [0; 3];
-        bc[0] = (word <<  8 >> 24) as u8;
-        bc[1] = (word << 16 >> 24) as u8;
-        bc[2] = (word << 24 >> 24) as u8;
-        ddata.push(bc[0]);
-        ddata.push(bc[1]);
-        ddata.push(bc[2]);
-    }
-    println!("{:?}", ddata);
-    let s: String = ddata.iter().map(|c| *c as char).collect();
-    println!("{:?}", s);
+    let decoded_data = decode(&encoded_string);
+    let decoded_string: String = decoded_data.iter().map(|c| *c as char).collect();
 
-    assert_eq!(string, s);
+    assert_eq!(string, decoded_string);
 }
